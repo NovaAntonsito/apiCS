e3d498dc5355e1f9b92b7ec8e7b59c4afd1e671d
Marcos Anton <antonbarroso16@gmail.com>
2023-04-23 22:31:50 -0300

    Otromas

--- src/services/authServices.ts
+++ src/services/authServices.ts
@@ -1,20 +1,19 @@
-import { auth } from '../controllers/interfaces/auth.interface';
+import { auth } from "../controllers/interfaces/auth.interface";
 import { getDataSource } from "../config/DBConfig";
 import { User } from "../models/user";
-import { Sucursales } from '../models/sucursal';
+import { Sucursales } from "../models/sucursal";
 import { encrypt, verifyPass } from "../utils/handlePassword";
-import { Repository } from 'typeorm';
-import { genToken } from '../utils/handleJWT'
-
+import { Repository } from "typeorm";
+import { genToken } from "../utils/handleJWT";
 
 let UserRepository: Repository<User>;
-let SucursalRepository : Repository<Sucursales>
+let SucursalRepository: Repository<Sucursales>;
 
 const initRepo = async () => {
   try {
     const appDataSource = await getDataSource();
     UserRepository = appDataSource.getRepository(User);
-    SucursalRepository = appDataSource.getRepository(Sucursales)
+    SucursalRepository = appDataSource.getRepository(Sucursales);
   } catch (error) {
     console.error(error);
     process.exit(1);
@@ -23,40 +22,48 @@ const initRepo = async () => {
 
 initRepo();
 
-const registerNewUser = async (username:string, password:any , email:string, surcursalNames:string[]) => {
-  try{
-  await initRepo()
-  const userFound = await UserRepository.findOne({where: { email, username }});
-  if (userFound) return false;
-  const sucursalesList: Sucursales[] = []
-  for(let i=0; i < surcursalNames.length;i++){
-  const sucursalFound = await SucursalRepository.findOne({where: { name: surcursalNames[i] }}) as Sucursales;
-  sucursalesList.push(sucursalFound)
-  }
-  const passHashed = await encrypt(password)
+const registerNewUser = async (
+  username: string,
+  password: any,
+  email: string,
+  surcursalNames: string[]
+) => {
+  try {
+    await initRepo();
+    const userFound = await UserRepository.findOne({
+      where: { email, username },
+    });
+    if (userFound) return false;
+    const sucursalesList: Sucursales[] = [];
+    for (let i = 0; i < surcursalNames.length; i++) {
+      const sucursalFound = (await SucursalRepository.findOne({
+        where: { name: surcursalNames[i] },
+      })) as Sucursales;
+      sucursalesList.push(sucursalFound);
+    }
+    const passHashed = await encrypt(password);
 
-  const newUser = UserRepository.create({
-    email,
-    password : passHashed,
-    username,
-    sucursales : sucursalesList
-  });
-  await UserRepository.save(newUser);
-  return newUser;
-  }catch(e){
-  console.log(e);
-  
-}
+    const newUser = UserRepository.create({
+      email,
+      password: passHashed,
+      username,
+      sucursales: sucursalesList,
+    });
+    await UserRepository.save(newUser);
+    return newUser;
+  } catch (e) {
+    console.log(e);
+  }
 };
 
-const loginUser = async ({email,password}:auth) => {
-    await initRepo()
-    const userFound = await UserRepository.findOne({where:{email}})
-    if(!userFound) return false;
-    const passValid = await verifyPass(userFound.password, password)
-    if(!passValid) return false;
-    const tokenJWT = genToken(userFound.username)
-    return tokenJWT;
+const loginUser = async ({ email, password }: auth) => {
+  await initRepo();
+  const userFound = await UserRepository.findOne({ where: { email } });
+  if (!userFound) return false;
+  const passValid = await verifyPass(userFound.password, password);
+  if (!passValid) return false;
+  const tokenJWT = genToken(userFound.username);
+  return tokenJWT;
 };
 
 export { registerNewUser, loginUser };
--- src/services/provinciaServices.ts
+++ src/services/provinciaServices.ts
@@ -1,68 +1,73 @@
-import {getDataSource} from '../config/DBConfig'
-import { Repository } from 'typeorm';
-import { Provincia} from '../models/provincia';
-import { Sucursales } from '../models/sucursal';
+import { getDataSource } from "../config/DBConfig";
+import { Repository } from "typeorm";
+import { Provincia } from "../models/provincia";
+import { Sucursales } from "../models/sucursal";
 
-
-
-
-let ProvinciaRepository : Repository<Provincia>
-let SucursalRepository : Repository<Sucursales>
+let ProvinciaRepository: Repository<Provincia>;
+let SucursalRepository: Repository<Sucursales>;
 
 const initRepo = async () => {
-    try {
-      const appDataSource = await getDataSource();
-      ProvinciaRepository = appDataSource.getRepository(Provincia);
-      SucursalRepository = appDataSource.getRepository(Sucursales)
-    } catch (error) {
-      console.error(error);
-      process.exit(1);
-    }
+  try {
+    const appDataSource = await getDataSource();
+    ProvinciaRepository = appDataSource.getRepository(Provincia);
+    SucursalRepository = appDataSource.getRepository(Sucursales);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
 };
 
-initRepo()
+initRepo();
 
-const createProvincia = async (nombre : string) =>{
-    await initRepo();
-    const provinciaFound = await ProvinciaRepository.findOne({where:{nombre}})
-    if(provinciaFound) return false;
-    const newProvincia = await ProvinciaRepository.create({nombre})    
-    await ProvinciaRepository.save(newProvincia)
-    return newProvincia
-}
+const createProvincia = async (nombre: string) => {
+  await initRepo();
+  const provinciaFound = await ProvinciaRepository.findOne({
+    where: { nombre },
+  });
+  if (provinciaFound) return false;
+  const newProvincia = await ProvinciaRepository.create({ nombre });
+  await ProvinciaRepository.save(newProvincia);
+  return newProvincia;
+};
 
 const viewAllProvincias = async () => {
- await initRepo();
- const provinciaFound = await ProvinciaRepository.find()
- if (provinciaFound.length === 0) return false
- return provinciaFound; 
-}
-
-const viewOneProvincia = async (nombre : string) =>{
   await initRepo();
-  const provinciaFound = await ProvinciaRepository.find({where:{nombre}})
-  if (!provinciaFound) return false
-  return provinciaFound 
-}
+  const provinciaFound = await ProvinciaRepository.find();
+  if (provinciaFound.length === 0) return false;
+  return provinciaFound;
+};
 
-const deleteProvincia = async (nombre : string) => {
-      await initRepo()
-      const provinciaFound = await ProvinciaRepository.findOne({where:{nombre}})
-      if (!provinciaFound) return false
-      const deletedProvincia = await ProvinciaRepository.delete(provinciaFound)
-      return true  
-}
+const viewOneProvincia = async (nombre: string) => {
+  await initRepo();
+  const provinciaFound = await ProvinciaRepository.find({ where: { nombre } });
+  if (!provinciaFound) return false;
+  return provinciaFound;
+};
 
+const deleteProvincia = async (nombre: string) => {
+  await initRepo();
+  const provinciaFound = await ProvinciaRepository.findOne({
+    where: { nombre },
+  });
+  if (!provinciaFound) return false;
+  const deletedProvincia = await ProvinciaRepository.delete(provinciaFound);
+  return true;
+};
 
-const updateProvincia = async (nombre : string, id : number) =>{
-      await initRepo()
-      const provinciaFound = await ProvinciaRepository.findOne({where:{id}})
-      if (!provinciaFound) return false
-      const provinciaToUpdate = await ProvinciaRepository.create({nombre})
-      const updatedProvincia = Object.assign(provinciaFound, provinciaToUpdate);
-      await ProvinciaRepository.save(updatedProvincia)
-      return updatedProvincia
-}
+const updateProvincia = async (nombre: string, id: number) => {
+  await initRepo();
+  const provinciaFound = await ProvinciaRepository.findOne({ where: { id } });
+  if (!provinciaFound) return false;
+  const provinciaToUpdate = await ProvinciaRepository.create({ nombre });
+  const updatedProvincia = Object.assign(provinciaFound, provinciaToUpdate);
+  await ProvinciaRepository.save(updatedProvincia);
+  return updatedProvincia;
+};
 
-
-export {createProvincia, viewAllProvincias, viewOneProvincia, deleteProvincia, updateProvincia}
+export {
+  createProvincia,
+  viewAllProvincias,
+  viewOneProvincia,
+  deleteProvincia,
+  updateProvincia,
+};
