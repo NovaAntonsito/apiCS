d6c47ceeba92a92e706a32f15c35c832c59c0c23
Marcos Anton <marcos.anton@alumnos.frm.utn.edu.ar>
2023-06-05 20:30:44 -0300

    Documentacion de api

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -8,11 +8,11 @@ import {handleUser} from "../middlewares/handleUserOAuth";
 const router = Router()
 
 router.post('/register', registerCtrl)
-
+//http://localhost:3000/api/auth/google (Llamada a google, void)
 router.get('/google', passport.authenticate('google', {failureRedirect:'/'}))
-
+//http://localhost:3000/api/auth/google/redirect (Redirect al backend )
 router.get('/google/redirect', passport.authenticate('google',{failureRedirect: '/'}),handleUser)
-
+//http://localhost:3000/api/auth/login (Login propio (email, password))
 router.post('/login', loginCtrl)
 
 
